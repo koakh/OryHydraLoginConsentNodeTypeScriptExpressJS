a5df0a440d7c5d558f8492b6fd8b64586d01aaba
Mário Monteiro <marioammonteiro@gmail.com>
2022-03-06 23:18:57 +0000

    try add permissions and roles

--- src/routes/consent.ts
+++ src/routes/consent.ts
@@ -50,6 +50,7 @@ router.get('/', csrfProtection, (req, res, next) => {
               // accessToken: { foo: 'bar' },
               // This data will be available in the ID token.
               // idToken: { baz: 'bar' },
+              id_token: { permissions: ['create:items', 'update:items', 'delete:items'], roles: ['ROLE_USER', 'ROLE_ADMIN'] }
             }
           })
           .then(({ data: body }) => {
