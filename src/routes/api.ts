a7da3484092585c605cfc37255fa704d9d378376
Mário Monteiro <marioammonteiro@gmail.com>
2022-07-30 01:39:58 +0100

    refactoring login and start api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -1,4 +1,4 @@
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 import { identityServerConfig } from '../config';
 import { LoginPayload, MessageResponse } from '../types';
 
@@ -7,20 +7,37 @@ const headers = {
   'Content-Type': 'application/json',
 };
 
-export const apiLogin = async (payload: LoginPayload): Promise<boolean> => {
-  try {
-    console.log(`payload: [${JSON.stringify(payload, undefined, 2)}]`);
-    const response = await axios.post<MessageResponse>(
-      `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
-      payload,
-      {
-        headers,
-      }
-    );
-    console.log(`response: [${JSON.stringify(response, undefined, 2)}]`);
-    return response.data.message === 'authorized';
-  } catch (error) {
-    console.error(`error: ${JSON.stringify(error, undefined, 2)}`);
+// export const apiLogin = async (payload: LoginPayload): Promise<boolean> => {
+//   try {
+//     console.log(`payload: [${JSON.stringify(payload, undefined, 2)}]`);
+//     const response = await axios.post<MessageResponse>(
+//       `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
+//       payload,
+//       {
+//         headers,
+//       }
+//     );
+//     console.log(`response: [${JSON.stringify(response, undefined, 2)}]`);
+//     return response.data.message === 'authorized';
+//   } catch (error) {
+//     console.error(`error: ${JSON.stringify(error, undefined, 2)}`);
+//     return false;
+//   }
+// };
+
+export const apiLogin = (payload: LoginPayload): boolean => {
+  console.log(`payload: [${JSON.stringify(payload, undefined, 2)}]`);
+  axios.post<MessageResponse>(
+    `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
+    payload,
+    {
+      headers,
+    }
+  ).then((data: AxiosResponse<MessageResponse, any>) => {
+    console.log(`data: [${JSON.stringify(data, undefined, 2)}]`);
+    return true;
+  }).catch((error) => {
+    console.log(`error: [${JSON.stringify(error, undefined, 2)}]`);
     return false;
   }
 };
