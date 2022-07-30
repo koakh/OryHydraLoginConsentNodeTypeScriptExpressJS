5d65f373f17b0591936a96f6f75f343bca195082
Mário Monteiro <marioammonteiro@gmail.com>
2022-07-30 02:04:15 +0100

    refactoring login and start api

--- src/routes/api.ts
+++ src/routes/api.ts
@@ -28,30 +28,36 @@ const headers = {
 export const apiLogin = async (
   payload: LoginPayload
 ): Promise<boolean | void> => {
-  const article = { title: 'Axios POST Request Example' };
-  const response = await axios.post(
-    `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
-    payload,
-    { headers }
-  );
-  console.log(`response: [${JSON.stringify(response.data, undefined, 2)}]`);
-
-  console.log(`payload: [${JSON.stringify(payload, undefined, 2)}]`);
-  axios
-    .post<MessageResponse>(
+  // const article = { title: 'Axios POST Request Example' };
+  const response = await axios
+    .post(
       `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
       payload,
-      {
-        headers,
-      }
+      { headers }
     )
-    .then((data: AxiosResponse<MessageResponse, any>) => {
-      console.log(`data: [${JSON.stringify(data, undefined, 2)}]`);
-      return true;
-    })
     .catch((error) => {
-      console.log(`error: [${JSON.stringify(error, undefined, 2)}]`);
+      console.error(error);
       return false;
     });
+  console.log(`response: [${JSON.stringify(response.data, undefined, 2)}]`);
+  return (response as any)?.message === 'authorized';
+
+  // console.log(`payload: [${JSON.stringify(payload, undefined, 2)}]`);
+  // axios
+  //   .post<MessageResponse>(
+  //     `${identityServerConfig.ccardIdentityServerUri}/api/citizens/login`,
+  //     payload,
+  //     {
+  //       headers,
+  //     }
+  //   )
+  //   .then((data: AxiosResponse<MessageResponse, any>) => {
+  //     console.log(`data: [${JSON.stringify(data, undefined, 2)}]`);
+  //     return true;
+  //   })
+  //   .catch((error) => {
+  //     console.log(`error: [${JSON.stringify(error, undefined, 2)}]`);
+  //     return false;
+  //   });
   // return true;
 };
